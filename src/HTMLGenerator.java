597e8e3ee4645d9e6add6f74b763ee1de8aa7878
renan tomazini <renantomazini@gmail.com>
2022-04-03 13:57:02 -0300

    CSS adicionado

--- src/HTMLGenerator.java
+++ src/HTMLGenerator.java
@@ -7,8 +7,18 @@ import java.util.List;
 public class HTMLGenerator {
 	
 	public void generate(List<Filme> Filmes) throws IOException {
+		String head =
+				"""
+				<head>
+					<meta charset=\"utf-8\">
+					<meta name=\"viewport\" content=\"width=device-width, initial-scale=1, shrink-to-fit=no\">
+					<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css\" 
+						+ "integrity=\"sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm\" crossorigin=\"anonymous\">					
+				</head>
+				""";
 		try {
 			Writer w = new FileWriter("output.html");  
+			w.write(head);
 			int n = Filmes.size();
 			for (int i=0; i<n; i++) {
 			      System.out.println("Titulo: " + Filmes.get(i).getTitulo());
@@ -18,7 +28,7 @@ public class HTMLGenerator {
 			      		+ Filmes.get(i).getUrlImage();			      		
 			      w.write(img + "\">");
 			      System.out.println(img);
-			      w.write("<p> Lançado em: " + Filmes.get(i).getYear() + "</p>");
+			      w.write("<p> Ano: " + Filmes.get(i).getYear() + "</p>");
 			      w.write("<p> Nota: " + Filmes.get(i).getRating() + "</p>");
 			      w.write("<br></br>");
 			    }
